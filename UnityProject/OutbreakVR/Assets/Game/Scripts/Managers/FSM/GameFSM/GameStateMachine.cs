9b9edb7b6dce7c4df22eab767e996b82ca7bcf84
FretBuzzBoogie <rohan31f@gmail.com>
2020-02-17 21:22:58 +0530

    - StateMachine.cs :: Made the script static, pushed all Scene transition code to child class GameStateMachine.cs

--- UnityProject/OutbreakVR/Assets/Game/Scripts/Managers/FSM/GameFSM/GameStateMachine.cs
+++ UnityProject/OutbreakVR/Assets/Game/Scripts/Managers/FSM/GameFSM/GameStateMachine.cs
@@ -19,6 +19,8 @@ namespace ns_Mashmo
             }
             s_Instance = this;
             base.initialize();
+
+            Transition(m_strStartState, SystemConsts.SCENE_NAME_INIT_SCENE, string.Empty);
         }
 
         public override void destroy()
@@ -40,6 +42,7 @@ namespace ns_Mashmo
         /// <summary>
         /// Transition to new state
         /// returns if the transition was successful
+        /// Static function to call from anywhere
         /// </summary>
         /// <param name="a_strNewState"></param>
         public static void Transition(string a_strNewState, string a_strSceneName, string a_strLevelName)
@@ -49,8 +52,58 @@ namespace ns_Mashmo
                 Debug.LogError("GameStateMachine::Transition:: ID of game state to transition to is empty.");
                 return;
             }
+            s_Instance.transition(a_strNewState, a_strSceneName, a_strLevelName, false);
+        }
 
-            s_Instance.transition(a_strNewState, a_strSceneName, a_strLevelName);
+        /// <summary>
+        /// Internal function to transition, switch to new scene before calling the new state enter
+        /// returns true if transition is allowed, else false
+        /// </summary>
+        /// <param name="a_strNewState"></param>
+        /// <param name="a_strSceneName"></param>
+        /// <param name="a_strLevelName"></param>
+        /// <param name="a_bIsTransitionToNewState"></param>
+        protected override bool transition(string a_strNewState, string a_strSceneName, string a_strLevelName, bool a_bIsTransitionToNewState = true)
+        {
+            if (!base.transition(a_strNewState, a_strSceneName, a_strLevelName, /*a_bIsTransitionToNewState is always false for this func*/ false))
+            {
+                return false;
+            }
+
+            EventHash l_EventHash = EventManager.GetEventHashtable();
+            l_EventHash.Add(GameEventTypeConst.ID_OLD_GAME_STATE, s_Instance.m_strLastState);
+            l_EventHash.Add(GameEventTypeConst.ID_NEW_GAME_STATE, s_Instance.m_strCurrentState);
+            EventManager.Dispatch(GAME_EVENT_TYPE.ON_GAME_STATE_ENDED, l_EventHash);
+
+            ///Changes the level being used by assets, i.e. task and objective
+            if (!string.IsNullOrEmpty(a_strLevelName))
+            {
+                EventHash l_Hashtable = EventManager.GetEventHashtable();
+                l_Hashtable.Add(GameEventTypeConst.ID_LEVEL_TYPE, a_strLevelName);
+                EventManager.Dispatch(GAME_EVENT_TYPE.ON_LEVEL_SELECTED, l_Hashtable);
+            }
+
+            ///Load scene will call the callback directly if already loaded
+            GameManager.LoadScene(a_strSceneName, s_Instance.onLevelSceneLoadComplete);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Callback on the level load scene complete
+        /// </summary>
+        private void onLevelSceneLoadComplete()
+        {
+            ManagedState l_NewManagedState = getRegisteredManagedState(m_strCurrentState);
+            if (l_NewManagedState != null)
+            {
+                l_NewManagedState.onStateEnter(m_strLastState);
+            }
+
+            EventHash l_EventHash = EventManager.GetEventHashtable();
+            l_EventHash.Add(GameEventTypeConst.ID_NEW_GAME_STATE, m_strCurrentState);
+            l_EventHash.Add(GameEventTypeConst.ID_OLD_GAME_STATE, m_strLastState);
+            EventManager.Dispatch(GAME_EVENT_TYPE.ON_GAME_STATE_STARTED, l_EventHash);
         }
     }
 }
--- UnityProject/OutbreakVR/Assets/Game/Scripts/Managers/FSM/StateMachine.cs
+++ UnityProject/OutbreakVR/Assets/Game/Scripts/Managers/FSM/StateMachine.cs
@@ -7,10 +7,10 @@ namespace ns_Mashmo
     public class StateMachine : AbsComponentHandler
     {
         [SerializeField]
-        private List<ManagedState> m_lstRegisteredManagedStates = null;
+        protected List<ManagedState> m_lstRegisteredManagedStates = null;
 
         [SerializeField]
-        private string m_strStartState = string.Empty;
+        protected string m_strStartState = string.Empty;
 
         [SerializeField]
         protected string m_strLastState = string.Empty;
@@ -19,11 +19,11 @@ namespace ns_Mashmo
         protected string m_strCurrentState = string.Empty;
 
         [SerializeField]
-        private bool m_bAllowTransitionToSelf = false;
+        protected bool m_bAllowTransitionToSelf = false;
 
         public override void initialize()
         {
-            transition(m_strStartState, SystemConsts.SCENE_NAME_INIT_SCENE, string.Empty);
+            
         }
 
         public override void destroy()
@@ -34,9 +34,10 @@ namespace ns_Mashmo
         /// <summary>
         /// Transitions to new state
         /// Fires an event on state changed
+        /// returns true if transition is allowed, else false
         /// </summary>
         /// <param name="a_strNewState"></param>
-        public void transition(string a_strNewState, string a_strSceneName, string a_strLevelName)
+        protected virtual bool transition(string a_strNewState, string a_strSceneName, string a_strLevelName, bool a_bIsTransitionToNewState = true)
         {
             string l_strOldStateId = m_strCurrentState;
             string l_strNewStateId = a_strNewState;
@@ -45,7 +46,7 @@ namespace ns_Mashmo
             
             if (!m_bAllowTransitionToSelf && l_strOldStateId.Equals(l_strNewStateId, System.StringComparison.OrdinalIgnoreCase))
             {
-                return;
+                return false;
             }
 
             m_strCurrentState = l_strNewStateId;
@@ -56,38 +57,16 @@ namespace ns_Mashmo
                 l_OldManagedState.onStateExit(l_strNewStateId);
             }
 
-            EventHash l_EventHash = EventManager.GetEventHashtable();
-            l_EventHash.Add(GameEventTypeConst.ID_OLD_GAME_STATE, m_strLastState);
-            l_EventHash.Add(GameEventTypeConst.ID_NEW_GAME_STATE, m_strCurrentState);
-            EventManager.Dispatch(GAME_EVENT_TYPE.ON_GAME_STATE_ENDED, l_EventHash);
-
-            ///Changes the level being used by assets, i.e. task and objective
-            if (!string.IsNullOrEmpty(a_strLevelName))
+            if (a_bIsTransitionToNewState)
             {
-                EventHash l_Hashtable = EventManager.GetEventHashtable();
-                l_Hashtable.Add(GameEventTypeConst.ID_LEVEL_TYPE, a_strLevelName);
-                EventManager.Dispatch(GAME_EVENT_TYPE.ON_LEVEL_SELECTED, l_Hashtable);
+                ManagedState l_NewManagedState = getRegisteredManagedState(m_strCurrentState);
+                if (l_NewManagedState != null)
+                {
+                    l_NewManagedState.onStateEnter(m_strLastState);
+                }
             }
 
-            ///Load scene will call the callback directly if already loaded
-            GameManager.LoadScene(a_strSceneName, onLevelSceneLoadComplete);
-        }
-
-        /// <summary>
-        /// Callback on the level load scene complete
-        /// </summary>
-        private void onLevelSceneLoadComplete()
-        {
-            ManagedState l_NewManagedState = getRegisteredManagedState(m_strCurrentState);
-            if (l_NewManagedState != null)
-            {
-                l_NewManagedState.onStateEnter(m_strLastState);
-            }
-
-            EventHash l_EventHash = EventManager.GetEventHashtable();
-            l_EventHash.Add(GameEventTypeConst.ID_NEW_GAME_STATE, m_strCurrentState);
-            l_EventHash.Add(GameEventTypeConst.ID_OLD_GAME_STATE, m_strLastState);
-            EventManager.Dispatch(GAME_EVENT_TYPE.ON_GAME_STATE_STARTED, l_EventHash);
+            return true;
         }
 
         /// <summary>
@@ -95,7 +74,7 @@ namespace ns_Mashmo
         /// </summary>
         /// <param name="a_strStateName"></param>
         /// <returns></returns>
-        private ManagedState getRegisteredManagedState(string a_strStateName)
+        protected ManagedState getRegisteredManagedState(string a_strStateName)
         {
             int l_iStateCount = m_lstRegisteredManagedStates.Count;
             for (int l_iStateIndex = 0; l_iStateIndex < l_iStateCount; l_iStateIndex++)
