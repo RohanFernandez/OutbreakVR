572b6f4141eca6692a8a28d995762aba606fe496
FretBuzzBoogie <rohan31f@gmail.com>
2020-05-27 17:27:47 +0530

    - Training :: setup all objectives for training

--- UnityProject/OutbreakVR/Assets/Game/Scripts/Managers/Training/LevelController_Training.cs
+++ UnityProject/OutbreakVR/Assets/Game/Scripts/Managers/Training/LevelController_Training.cs
@@ -8,14 +8,27 @@ namespace ns_mashmo
     public class LevelController_Training : MonoBehaviour
     {
         /// <summary>
-        /// the objective trigger that will be fired on reloading during the training
+        /// the objective trigger that will be fired on reloading during the training for primary weapon
         /// </summary>
         [SerializeField]
-        string m_strObjTriggerOnReload = string.Empty;
+        string m_strObjTriggerOnReload_Primary = string.Empty;
+
+        /// <summary>
+        /// the objective trigger that will be fired on reloading during the training for secondary weapon
+        /// </summary>
+        [SerializeField]
+        string m_strObjTriggerOnReload_Secondary = string.Empty;
+
+        /// <summary>
+        /// the objective trigger that will be fired swithcing the weapon category
+        /// </summary>
+        [SerializeField]
+        string m_strObjTriggerWeaponCategoryChanged = string.Empty;
 
         void Awake()
         {
             EventManager.SubscribeTo(GAME_EVENT_TYPE.ON_WEAPON_RELOADED, onWeaponReloaded);
+            EventManager.SubscribeTo(GAME_EVENT_TYPE.ON_CURRENT_WEAPON_OR_CATEGORY_CHANGED, onWeaponOrCategoryChanged);
         }
 
         void OnDestroy()
@@ -29,7 +42,32 @@ namespace ns_mashmo
         /// <param name="a_Event"></param>
         void onWeaponReloaded(EventHash a_Event)
         {
-            ObjectiveManager.TriggerObjective(m_strObjTriggerOnReload);
+            WEAPON_TYPE l_WeaponType = (WEAPON_TYPE)a_Event[GameEventTypeConst.ID_WEAPON_TYPE];
+            WEAPON_CATEGORY_TYPE l_WeaponCategory = WeaponManager.GetCategory(l_WeaponType);
+
+            if (l_WeaponCategory == WEAPON_CATEGORY_TYPE.PRIMARY)
+            {
+                ObjectiveManager.TriggerObjective(m_strObjTriggerOnReload_Primary);
+            }
+            else if (l_WeaponCategory == WEAPON_CATEGORY_TYPE.SECONDARY)
+            {
+                ObjectiveManager.TriggerObjective(m_strObjTriggerOnReload_Secondary);
+            }
+        }
+
+        /// <summary>
+        /// event callback on weapon or category changed
+        /// </summary>
+        /// <param name="a_Event"></param>
+        void onWeaponOrCategoryChanged(EventHash a_Event)
+        {
+            WEAPON_CATEGORY_TYPE l_WeaponNewCategoryType = (WEAPON_CATEGORY_TYPE)a_Event[GameEventTypeConst.ID_NEW_WEAPON_CATEGORY_TYPE];
+            WEAPON_CATEGORY_TYPE l_WeaponOldCategoryType = (WEAPON_CATEGORY_TYPE)a_Event[GameEventTypeConst.ID_OLD_WEAPON_CATEGORY_TYPE];
+
+            if (l_WeaponOldCategoryType != l_WeaponNewCategoryType)
+            {
+                ObjectiveManager.TriggerObjective(m_strObjTriggerWeaponCategoryChanged);
+            }
         }
     }
 }
