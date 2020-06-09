b34f5aa1ecd0a90e9c338f565ce973a7c29d847d
FretBuzzBoogie <rohan31f@gmail.com>
2020-06-09 21:44:05 +0530

    - Credits :: setup scene with the panel

---
+++ UnityProject/OutbreakVR/Assets/Game/Scripts/Managers/Credits/LevelController_Credits.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ns_Mashmo
+{
+    public class LevelController_Credits : MonoBehaviour
+    {
+        [SerializeField]
+        private Scrollbar m_CreditsScrollbar = null;
+
+        [SerializeField]
+        private float m_fScrollSpeed = 0.08f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            m_CreditsScrollbar.value += Time.deltaTime * m_fScrollSpeed;
+
+            if (m_CreditsScrollbar.value >= 1.0f)
+            {
+                gameObject.SetActive(false);
+                onScrollCompleted();
+            }
+        }
+
+        /// <summary>
+        /// On scrolling completed go back to home
+        /// </summary>
+        private void onScrollCompleted()
+        {
+            LevelManager.GoToLevel(GameConsts.STATE_NAME_HOME);
+        }
+    }
+}
