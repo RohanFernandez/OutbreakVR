c519b4cc8e0b4b842eb93a6b4b6bb6cccdc2cf91
Anuj Kansara <ig.anujknsr@gmail.com>
2020-10-03 01:12:40 +0530

    Optimising level 1 in progress, Created backup scenes for home and level 1. Added new remove blend probe script given by Ajith.(Tested and works fine). Home and training further optimised. Updated a lot of prefabs with reflection probes value turned off. Baked entire level 1 with culling and final settings. (Level 1 still has issues and will be fixed in the next update). Updated reflection probes for level 1. Uncompressed some textures for home and training for testing purpose as drawcalls have been reduced now. Updated nav mesh for level 1.

---
+++ UnityProject/OutbreakVR/Assets/Test/Editor/DisableBlendProbesWindow.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Outbreak
+{
+    public class ListWrapper : UnityEngine.Object
+    {
+        public List<GameObject> listExceptions = new List<GameObject>();
+    }
+    public class DisableBlendProbesWindow : EditorWindow
+    {
+        [MenuItem("BlendProbes/Disable Probes")]
+
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(DisableBlendProbesWindow));
+        }
+
+        ListWrapper listWrapper = new ListWrapper();
+        void OnGUI()
+        {
+            listWrapper = (ListWrapper)EditorGUILayout.ObjectField(listWrapper, listWrapper.GetType(), true);
+
+            if (GUILayout.Button("Disable probes"))
+            {
+                List<MeshRenderer> meshesInScene = new List<MeshRenderer>();
+                MeshRenderer[] meshRenderers = (MeshRenderer[])Resources.FindObjectsOfTypeAll(typeof(MeshRenderer)) ;// FindObjectsOfType<MeshRenderer>();
+
+                foreach (MeshRenderer mr in meshRenderers)
+                {
+                    if (!EditorUtility.IsPersistent(mr.gameObject.transform.root.gameObject) 
+                        && !(mr.gameObject.hideFlags == HideFlags.NotEditable 
+                        || mr.gameObject.hideFlags == HideFlags.HideAndDontSave))
+                        meshesInScene.Add(mr);
+                }
+
+                foreach (var mr in meshesInScene)
+                {
+                    bool bFound = false;
+                    foreach (GameObject g in listWrapper.listExceptions)
+                    {
+                        if (g == mr.gameObject)
+                        {
+                            bFound = true;
+                            break;
+                        }
+                    }
+
+                    if (bFound)
+                    {
+                        continue;
+                    }
+
+                    //Undo.RecordObject(mr.gameObject, "Disabled reflection probe");
+                    Undo.RegisterCompleteObjectUndo(mr.gameObject, "Disabled reflection probe");
+                    Undo.FlushUndoRecordObjects();
+                    mr.lightProbeUsage = UnityEngine.Rendering.LightProbeUsage.Off;
+                    mr.reflectionProbeUsage = UnityEngine.Rendering.ReflectionProbeUsage.Off;
+
+                    
+                }
+            }
+        }
+    }
+}
